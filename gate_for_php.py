ddea806ac1461e86996b8e6323b23383aca743a7
oleg134514 <168273249+oleg134514@users.noreply.github.com>
2025-03-11 21:56:40 +0500

    Create gate_for_php.py

---
+++ gate_for_php.py
@@ -0,0 +1,11 @@
+import sys
+name = sys.stdin.readline().strip()
+phone = sys.stdin.readline().strip()
+ip = sys.stdin.readline().strip()
+'''
+тестовый блок
+text_file = open("Output.txt", "w")
+text_file.write(name + ' ' + phone + ' ' + ip)
+text_file.close()
+'''
+sys.exit(0) # Указывает на успешное завершение
